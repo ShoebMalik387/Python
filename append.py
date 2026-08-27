cfbbe4ac3c93f09d36fe314fc9a1796087a8eb56
ShoebMalik387 <malikshoeb924@gmail.com>
2026-08-27 22:49:58 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,27 @@
+l=[1,2,3]
+l.append("rahul")
+print (l)
+
+
+#insert method:
+
+l=[1,4,3,7,6,4,4,3,6]
+l.insert(4,9)
+print (l)
+
+
+#Extend position:
+
+L1=[6,5,4]
+L2=[3,2,1]
+L1.extend (L2)
+print (L1) 
+print (L2)
+
+
+#append:
+l1=[4,5,6]
+l2=[3,4,5]
+l1.append(l2)
+print (l1)
+l1.extend(l)

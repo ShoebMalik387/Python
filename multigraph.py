a3527daa8a67fdf7c1843ac05ea2c6b221826252
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-12 00:11:17 +0530

    Add files via upload

---
+++ multigraph.py
@@ -0,0 +1,18 @@
+
+import matplotlib.pyplot as plt
+
+student = ['A','B','C']
+
+marks1 = [30,40,50]
+
+marks2 = [60,70,90]
+
+plt.bar(student,marks1)
+
+plt.bar(student,marks2)
+
+plt.show ()
+
+
+
+
